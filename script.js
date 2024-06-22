dd596a876d7bb82f05ab7eeec8cae81e17c015a8
spyguel <pabloperezdisanti@gmail.com>
2024-06-22 08:21:27 -0300

    Se implemetó el callback para separar la función de sacar un arreglo del input

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 
 
-function media() {
+
+
+/*function media() {
     const inputText = document.getElementById('miInput').value;
     const elementos = inputText.split(',').map(Number);
     let total = 0;
     for (let i = 0; i < elementos.length; i++) {
         total += elementos[i];
     }
+    console.log(elementos)
     
-    const res = total / elementos.length;
-    document.write(res);
+    
+    const resMedia = total / elementos.length;
+
+
+    document.getElementById('media').innerHTML = resMedia
+
+    elementos.sort((x, y) => x - y);
+    let mitad = Math.floor(elementos.length /2)
+    console.log(elementos.length)
+
+    const resMediana = elementos.length % 2 === 1
+    ? elementos[mitad]
+    : (elementos[mitad - 1] + elementos[mitad]) / 2;
+  
+    document.getElementById('mediana').innerHTML = resMediana
+
+}*/
+
+
+function arreglo(segundo) {
+    const inputText = document.getElementById('miInput').value;
+    elementos = inputText.split(',').map(Number);
+    let total = 0;
+    for (let i = 0; i < elementos.length; i++) {
+        total += elementos[i];
+    }
+    segundo();
 }
+
+function segundo() {
+    console.log(elementos);
+}
+
 
