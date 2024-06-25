5de4eac443bc670c74b86d471449162fe4ae8280
spyguel <pabloperezdisanti@gmail.com>
2024-06-25 05:15:32 -0300

    Se dividió el código en partes más pequeñas

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
 
-function arreglo(segundo) {
+function verificar() {
     inputText = document.getElementById('miInput').value;
-    elementos = inputText.split(',').map(Number);
-    console.log(elementos)
-    segundo();
+    const regla = /^(\d+,)+\d+$/;
+    if (regla.test(inputText)) {
+        elementos = inputText.split(',').map(Number);
+    } else {
+        document.getElementById('alerta').innerHTML = "Debe ingresar un conjunto de datos. por ejemplo: '1,2,3,4'";
+        document.getElementById('resultado').style.visibility = "hidden";
+    }
 }
 
-
-
-function segundo(){
+function media() {
+    verificar();
+    if (elementos.length === 0) return;
+    document.getElementById('miInput').value = null; 
     
     let total = 0;
     for (let i = 0; i < elementos.length; i++) {
         total += elementos[i];
     }
-    
-    
+
     const resMedia = total / elementos.length;
+    limpiar()
+    document.getElementById('media').innerHTML = "Media: " + resMedia;
+    document.getElementById('resultado').style.visibility = "visible";
+    elementos.length = 0
+}
 
-
-    document.getElementById('media').innerHTML = "Media: "+resMedia
-
-    let variable = 0, contador = 0, cuenta = 0
+function moda() {
+    verificar();
+    if (elementos.length === 0) return;
+    document.getElementById('miInput').value = null; 
+    
+    let variable = 0, contador = 0, cuenta = 0;
     elementos.map(p => {
-        cuenta = 0
+        cuenta = 0;
         elementos.map(x => {
-            if (p == x) { cuenta++ }
-        })
+            if (p == x) { cuenta++; }
+        });
         if (cuenta > contador) {
+            contador = cuenta;
             variable = p;
         }
     });
-
-    document.getElementById('moda').innerHTML = "Moda: " +variable
-
-
+    limpiar()
+    document.getElementById('moda').innerHTML = "Moda: " + variable;
+    document.getElementById('resultado').style.visibility = "visible";
+    elementos.length = 0
+}
+function mediana(){
+    verificar()
+    if (elementos.length === 0) return;
+    document.getElementById('miInput').value = null; 
     elementos.sort((x, y) => x - y);
     let mitad = Math.floor(elementos.length /2)
-    console.log(elementos.length)
 
     const resMediana = elementos.length % 2 === 1
     ? elementos[mitad]
     : (elementos[mitad - 1] + elementos[mitad]) / 2;
-  
+
+    limpiar()
     document.getElementById('mediana').innerHTML = "Mediana: "+resMediana
-
     document.getElementById('resultado').style.visibility = "visible"
+    elementos.length = 0
+}
 
+function limpiar() {
+    document.getElementById('moda').innerHTML = "" 
+    document.getElementById('media').innerHTML = ""
+    document.getElementById('mediana').innerHTML = ""
 }
